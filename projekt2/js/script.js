1fafa93cc0ec8c96b67eff01f00da2fa7de411b2
masterfiletable2 <masterfiletable2@gmail.com>
2019-03-05 21:17:05 +0100

    projekt_2_before_final

--- projekt2/js/script.js
+++ projekt2/js/script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', appStart);
 
 const sounds={
@@ -20,16 +21,31 @@ const channels={
    }
 
 
+
+
+
+   let checkList = document.querySelectorAll(".channel__check");
+   let radioList = document.querySelectorAll(".channel__radio")   
+
+
+
+
 function appStart(){
 
     window.addEventListener('keypress',readKey);
 
-   
-   let divs = document.querySelectorAll(".sounds__button");
+ 
+    document.querySelector('#record').addEventListener('click',recAudio);
+    document.querySelector('#play').addEventListener('click',playAudio);
 
+   
+
+  
+
+    
     }   
     
-    //funkcja przekazujaca klawisz do zmiennej soundTitle, odtwarzajaca dzwiek za pomoca innej funkcji playSound
+   
         function readKey(e){
             e.preventDefault();
         
@@ -49,4 +65,112 @@ function appStart(){
         
     }
 
+
+
+
+
     
+    
+function playAudio(){  
+   
+   
+
+    for(let j=0;j<checkList.length;j++){
+        
+           if(checkList[j].checked===true){
+             
+          
+          
+
+         channels["channel"+(j+1)].forEach(sound =>{  
+             setTimeout(
+             
+                 ()=>{ 
+                  const soundObject = document.querySelector(`#${sound.name}`);
+                  soundObject.currentTime=0;
+                  soundObject.play();
+                 },sound.time
+          
+             )
+      
+      })
+     } 
+     
+    }
+
+         
+     
+ 
+ }
+
+
+
+
+ let recording = false;
+ let startTimerRecord = 0; 
+
+function recAudio(event){
+    event.preventDefault();
+    event.stopPropagation();
+recording=!recording;
+startTimerRecord=Date.now();
+event.target.innerHTML=recording?'◼':'◉';
+if(recording){
+    event.target.style.background="#ff0000";
+}
+else{
+    event.target.style.background="#e5e5e5";
+}
+}
+
+
+
+function readKey(e){
+    e.preventDefault();
+
+if(!sounds[e.charCode]){
+    return
+}
+
+ const soundName = sounds[e.charCode];
+
+
+
+playSound(soundName);
+}
+
+
+function playSound(soundName){
+ soundObject = document.querySelector(`#${soundName}`);
+ soundObject.currentTime=0; 
+ soundObject.play();
+ 
+
+
+
+if(recording){
+ 
+    for(let i=0;i<radioList.length;i++){
+
+        if(radioList[i].checked===true){
+    
+      channels["channel"+(i+1)].push(
+          {
+      name: soundName,
+      time:Date.now() - startTimerRecord
+      }
+     )
+ }
+  
+      }
+}
+
+}
+
+
+/*
+function addChannell(){
+    let channel__list = document.querySelector('.channel__list')
+    channel__list.appendChild(" <li class='channel__list__element'> <input type='checkbox' class='check radio-check' name='channel4' data-channel='channel4'>Channel 4</li>")
+}
+*/
